6ad7317a805017333e054f1a20c9fdea0b1a928c
CarlotaRob <carlotaroblesuab@gmail.com>
2026-01-04 19:14:56 +0100

    Fix variable name in majuscula function

--- formulari.js
+++ formulari.js
@@ -1,7 +1,8 @@
 let nombre = document.getElementById("nom").value;
 function majuscula(){
-    document.getElementById("nom").value = nom.toUpperCase(primeralletra);
+    document.getElementById("nom").value = nom.toUpperCase(primeraLletra);
     }
+
 
 
 
