9628f9f6edcaa8a7cef68299c8ec669698d4a362
CarlotaRob <carlotaroblesuab@gmail.com>
2026-01-04 19:50:21 +0100

    Update formulari.js

--- formulari.js
+++ formulari.js
@@ -33,3 +33,23 @@ inputNom.onblur = function() {
     }
     this.value = paraules.join(" ");
 };
+
+
+
+function corregirMajuscules() {
+    let nomInput = document.getElementById("nom");
+    let cognomInput = document.getElementById("cognom");
+
+    let nom = nomInput.value;
+    let cognom = cognomInput.value;
+
+    if (nom.length > 0) {
+        nom = nom[0].toUpperCase() + nom.substring(1);
+        nomInput.value = nom;
+    }
+
+    if (cognom.length > 0) {
+        cognom = cognom[0].toUpperCase() + cognom.substring(1);
+        cognomInput.value = cognom;
+    }
+}
